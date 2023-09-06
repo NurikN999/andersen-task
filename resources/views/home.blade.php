3660c62ebdc520bd8c4329de47251c26081c7afd
nurmukhamed <nurkamalnurmukhamed@gmail.com>
2023-09-06 18:49:17 +0500

    feat: added error alert

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -26,6 +26,15 @@
                     </form>
                 </div>
             </div>
+            @if ($errors->any())
+                <div class="alert alert-danger mt-4">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
 
         </div>
     </div>
